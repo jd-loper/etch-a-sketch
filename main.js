cd4c53d58bc21208a4216d71a32b94977b48e857
Joseph Loper <jd.loper@icloud.com>
2024-04-26 12:13:55 -0700

    Add functionality to clear canvas

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const container = document.querySelector("#container");
+const clearBtn = document.querySelector("#clearBtn");
 
 for (let i = 0; i < 16 * 16; i++) {
   const div = document.createElement("div");
+  div.classList.add("cell");
   div.style.border = "1px solid black";
   div.style.flex = "1 0 calc(100% / 16)";
   div.style.boxSizing = "border-box";
@@ -13,3 +15,12 @@ for (let i = 0; i < 16 * 16; i++) {
 
   container.appendChild(div);
 }
+
+function clearBoard() {
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.style.backgroundColor = "";
+  });
+}
+
+clearBtn.addEventListener("click", clearBoard);
