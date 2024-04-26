b9ba951986b08528db511495709c6efba6f3cb4b
Joseph Loper <jd.loper@icloud.com>
2024-04-26 12:37:11 -0700

    Prompt user to define canvas size

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
 const container = document.querySelector("#container");
 const clearBtn = document.querySelector("#clearBtn");
 
-for (let i = 0; i < 16 * 16; i++) {
+let boardSize;
+
+do {
+  boardSize = prompt("Define the canvas size (e.g. 16 for a 16x16 canvas):");
+
+  boardSize = Number(boardSize);
+
+  if (
+    isNaN(boardSize) ||
+    boardSize < 1 ||
+    boardSize > 100 ||
+    !Number.isInteger(boardSize)
+  ) {
+    alert("Enter a positive number less than or equal to 100.");
+  }
+} while (
+  isNaN(boardSize) ||
+  boardSize < 1 ||
+  boardSize > 100 ||
+  !Number.isInteger(boardSize)
+);
+
+for (let i = 0; i < boardSize * boardSize; i++) {
   const div = document.createElement("div");
   div.classList.add("cell");
-  div.style.border = "1px solid black";
-  div.style.flex = "1 0 calc(100% / 16)";
+  div.style.border = ".5px solid lightgray";
+  div.style.flex = `1 0 calc(100% / ${boardSize})`;
   div.style.boxSizing = "border-box";
   div.style.aspectRatio = "1/1";
 
