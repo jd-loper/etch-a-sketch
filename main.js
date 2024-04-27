ec44d593c527b5634d0e111c445dc1d16ada48df
Joseph Loper <jd.loper@icloud.com>
2024-04-26 21:16:36 -0700

    Add functionality to change canvas size

--- main.js
+++ main.js
@@ -1,27 +1,8 @@
 const container = document.querySelector("#container");
 const clearBtn = document.querySelector("#clearBtn");
+const canvasBtn = document.querySelector("#canvasBtn");
 
-let boardSize;
-
-do {
-  boardSize = prompt("Define the canvas size (e.g. 16 for a 16x16 canvas):");
-
-  boardSize = Number(boardSize);
-
-  if (
-    isNaN(boardSize) ||
-    boardSize < 1 ||
-    boardSize > 100 ||
-    !Number.isInteger(boardSize)
-  ) {
-    alert("Enter a positive number less than or equal to 100.");
-  }
-} while (
-  isNaN(boardSize) ||
-  boardSize < 1 ||
-  boardSize > 100 ||
-  !Number.isInteger(boardSize)
-);
+let boardSize = 16;
 
 for (let i = 0; i < boardSize * boardSize; i++) {
   const div = document.createElement("div");
@@ -37,6 +18,49 @@ for (let i = 0; i < boardSize * boardSize; i++) {
 
   container.appendChild(div);
 }
+
+function changeCanvas() {
+  container.innerHTML = "";
+  let newBoardSize;
+
+  do {
+    newBoardSize = prompt(
+      "Define the canvas size (e.g. 16 for a 16x16 canvas):"
+    );
+
+    newBoardSize = Number(newBoardSize);
+
+    if (
+      isNaN(newBoardSize) ||
+      newBoardSize < 1 ||
+      newBoardSize > 100 ||
+      !Number.isInteger(newBoardSize)
+    ) {
+      alert("Enter a positive number less than or equal to 100.");
+    }
+  } while (
+    isNaN(newBoardSize) ||
+    newBoardSize < 1 ||
+    newBoardSize > 100 ||
+    !Number.isInteger(newBoardSize)
+  );
+  boardSize = newBoardSize;
+
+  for (let i = 0; i < boardSize * boardSize; i++) {
+    const div = document.createElement("div");
+    div.classList.add("cell");
+    div.style.border = ".5px solid lightgray";
+    div.style.flex = `1 0 calc(100% / ${boardSize})`;
+    div.style.boxSizing = "border-box";
+    div.style.aspectRatio = "1/1";
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "black";
+    });
+    container.appendChild(div);
+  }
+}
+
+canvasBtn.addEventListener("click", changeCanvas);
 
 function clearBoard() {
   const cells = document.querySelectorAll(".cell");
